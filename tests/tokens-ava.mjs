082a4d2c12a24986c8a2f21456709bfc19f8b2cd
Markus Felten <Markus.Felten@gmx.de>
2023-11-02 17:48:10 +0000

    test: separate token tests

---
+++ tests/tokens-ava.mjs
@@ -0,0 +1,49 @@
+import test from "ava";
+import { tokens, getAttribute } from "pacc";
+
+function tt(t, input, expected) {
+  try {
+    const result = [...tokens(input)];
+    t.deepEqual(result, expected);
+  } catch (e) {
+    t.deepEqual(e, expected);
+  }
+}
+
+tt.title = (providedTitle = "token", input, expected) =>
+  `${providedTitle} ${input}${
+    expected instanceof Error ? " =>ERROR" : ""
+  }`.trim();
+
+test(tt, '"a', new Error("unterminated string"));
+
+test(tt, " 'a'b\"c\" ", ["a", "b", "c"]);
+test(tt, " 'a\\\\\\n\\r' ", ["a\\\n\r"]);
+
+test(tt, "a< <= >= b>", ["a", "<", "<=", ">=", "b", ">"]);
+test(tt, "a=", ["a", "="]);
+test(tt, "a!=", ["a", "!="]);
+test(tt, "a>=", ["a", ">="]);
+test(tt, "a<=", ["a", "<="]);
+test(tt, "a[ 2 ] .c", ["a", "[", "2", "]", ".", "c"]);
+test(tt, "a123 <= >= a = <> +-[]() b.c 1234567890", [
+  "a123",
+  "<=",
+  ">=",
+  "a",
+  "=",
+  "<",
+  ">",
+  "+",
+  "-",
+  "[",
+  "]",
+  "(",
+  ")",
+  "b",
+  ".",
+  "c",
+  "1234567890"
+]);
+
+test(tt, "a[*]._b", ["a", "[", "*", "]", ".", "_b"]);
