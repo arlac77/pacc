f5ecc19df5a23b8d2bc1a7e6f7fb3441a349c636
Markus Felten <Markus.Felten@gmx.de>
2025-09-16 18:29:03 +0100

    test: more expansion type tests

--- tests/expand-ava.mjs
+++ tests/expand-ava.mjs
@@ -36,3 +36,19 @@ test("plain expand string transitive", t => {
 
   t.is(expand("${a}", context), 2);
 });
+
+test("expand undefined", t => t.is(expand(undefined, {}), undefined));
+test("expand null", t => t.is(expand(null, {}), null));
+test("expand NaN", t => t.is(expand(NaN, {}), NaN));
+test("expand false", t => t.is(expand(false, {}), false));
+test("expand true", t => t.is(expand(true, {}), true));
+test("expand number", t => t.is(expand(42, {}), 42));
+test("expand bigint", t => t.is(expand(43n, {}), 43n));
+test("expand function", t => {
+  function f() {}
+  t.is(expand(f, {}), f);
+});
+test("expand Date", t => {
+  const d = new Date();
+  t.is(expand(d,{}), d);
+});
