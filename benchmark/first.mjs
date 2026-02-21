a43162eb5365bef4f74d887c0c7780b4a4a08963
Markus Felten <Markus.Felten@gmx.de>
2026-02-21 11:19:23 +0000

    chore: more benchmarks

--- benchmark/first.mjs
+++ benchmark/first.mjs
@@ -4,11 +4,11 @@ import { parse } from "../src/parser.mjs";
 
 const root = new Map([
   ["a", { n: 1, l: [1, 2] }],
-  ["b", { n: 3, x: 7, l: [3, 4] }]
+  ["b", { n: 3, x: 7, l: [3, 4] }],
+  ["c", { d: { e: { f: { g: "xyz" } }} }]
 ]);
 
-bench('"[n<5].l"', () => {
-  const result = parse("[n<5].l", { root });
-});
+bench('"[n<5].l"', () => parse("[n<5].l", { root }));
+bench('"c.d.e.f.g"', () => parse("c.d.e.f.g", { root }));
 
 await run();
