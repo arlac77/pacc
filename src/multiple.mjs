40852c8897997328019336302197d79d91179489
Markus Felten <Markus.Felten@gmx.de>
2026-06-26 21:57:00 +0100

    refactor: use attribute as var name

--- src/multiple.mjs
+++ src/multiple.mjs
@@ -1,6 +1,7 @@
 import { setAttribute, getAttribute } from "./settergetter.mjs";
 import { attributeIterator, toExternal } from "./attributes.mjs";
 import { default_attribute } from "./common-attributes.mjs";
+
 /**
  * Copies attribute values from a source object into a destination object.
  * @param {Object} object target object to be modified
@@ -9,25 +10,25 @@ import { default_attribute } from "./common-attributes.mjs";
  * @param {function?} cb callback to be executed for each copied value
  */
 export function setAttributes(object, source, definitions, cb) {
-  for (const [path, def] of attributeIterator(definitions)) {
+  for (const [path, attribute] of attributeIterator(definitions)) {
     const name = path.join(".");
 
     let value = getAttribute(source, name);
 
     if (value === undefined) {
       if (
-        def.default === undefined ||
+        attribute.default === undefined ||
         getAttribute(object, name) !== undefined
       ) {
         continue;
       }
-      value = def.default;
+      value = attribute.default;
     }
 
-    setAttribute(object, name, value, def);
+    setAttribute(object, name, value, attribute);
 
     if (cb) {
-      cb(def, name, value);
+      cb(attribute, name, value);
     }
   }
 }
@@ -42,12 +43,11 @@ export function setAttributes(object, source, definitions, cb) {
 export function getAttributes(object, definitions, filter) {
   const result = {};
 
-  for (const [path, def] of attributeIterator(definitions, filter)) {
+  for (const [path, attribute] of attributeIterator(definitions, filter)) {
     const name = path.join(".");
-
-    const value = getAttribute(object, name, def);
+    const value = getAttribute(object, name, attribute);
     if (value !== undefined) {
-      setAttribute(result, name, value, def);
+      setAttribute(result, name, value, attribute);
     }
   }
   return result;
@@ -63,16 +63,16 @@ export function getAttributes(object, definitions, filter) {
 export function getAttributesJSON(object, definitions, filter) {
   const result = {};
 
-  for (const [path, def] of attributeIterator(definitions, filter)) {
+  for (const [path, attribute] of attributeIterator(definitions, filter)) {
     const name = path.join(".");
 
-    let value = getAttribute(object, name, def);
+    let value = getAttribute(object, name, attribute);
     if (value !== undefined) {
       value = toExternal(value);
       if (value instanceof Set) {
         value = [...value];
       }
-      setAttribute(result, def.externalName ?? name, value, default_attribute);
+      setAttribute(result, attribute.externalName ?? name, value, default_attribute);
     }
   }
   return result;
