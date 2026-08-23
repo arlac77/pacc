363235f380185dd8069b284e296357f840d7f256
Markus Felten <Markus.Felten@gmx.de>
2026-08-23 12:33:24 +0100

    test: cleanup

--- tests/as-array-ava.mjs
+++ tests/as-array-ava.mjs
@@ -2,9 +2,9 @@ import test from "ava";
 import { asArray } from "pacc";
 
 test("asArray from scalar", t => t.deepEqual(asArray(1), [1]));
+test("asArray from string", t => t.deepEqual(asArray("abc"), ["abc"]));
 test("asArray from array", t => t.deepEqual(asArray([1]), [1]));
 test("asArray from undefined", t => t.deepEqual(asArray(undefined), []));
-test("asArray from string", t => t.deepEqual(asArray("abc"), ["abc"]));
 test("asArray from Map", t =>
   t.deepEqual(asArray(new Map([["a", 1]])), [["a", 1]]));
 test("asArray from Set", t =>
