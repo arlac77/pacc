af0168417c704b0d84a58c5abec2e96faf35f5a7
Markus Felten <Markus.Felten@gmx.de>
2026-09-04 12:27:45 +0100

    test: arraz from AggregatedMap

--- tests/as-array-ava.mjs
+++ tests/as-array-ava.mjs
@@ -1,4 +1,5 @@
 import test from "ava";
+import { AggregatedMap } from "aggregated-map";
 import { asArray } from "pacc";
 
 test("asArray from scalar", t => t.deepEqual(asArray(1), [1]));
@@ -7,5 +8,13 @@ test("asArray from array", t => t.deepEqual(asArray([1]), [1]));
 test("asArray from undefined", t => t.deepEqual(asArray(undefined), []));
 test("asArray from Map", t =>
   t.deepEqual(asArray(new Map([["a", 1]])), [["a", 1]]));
+test("asArray from AggregatedMap", t =>
+  t.deepEqual(
+    asArray(new AggregatedMap([new Map([["a", 1]]), new Map([["b", 2]])])),
+    [
+      ["a", 1],
+      ["b", 2]
+    ]
+  ));
 test("asArray from Set", t =>
   t.deepEqual(asArray(new Set(["a", "b"])), ["a", "b"]));
